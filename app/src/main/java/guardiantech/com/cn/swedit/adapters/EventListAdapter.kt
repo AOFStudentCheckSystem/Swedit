99c0f21400c98cf44712a822c45faae8bc9bd34d
liupeiqi <peiqial@outlook.com>
2017-04-21 19:46:18 -0400

    workign

--- app/src/main/java/guardiantech/com/cn/swedit/adapters/EventListAdapter.kt
+++ app/src/main/java/guardiantech/com/cn/swedit/adapters/EventListAdapter.kt
@@ -1,5 +1,6 @@
 package guardiantech.com.cn.swedit.adapters
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
@@ -12,21 +13,26 @@ import guardiantech.com.cn.swedit.R
 /**
  * Created by liupeiqi on 2017/4/20.
  */
-class EventListAdapter : BaseAdapter() {
+class EventListAdapter(private val context: Context) : BaseAdapter() {
+
+    val fakeData = listOf("1","2","3","1","2","3","1","2","3","1","2","3","1","2","3")
+
+    @SuppressLint("InflateParams", "ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        convertView ?: LayoutInflater.from(context).inflate(R.layout.event_list_item, null)
+
     }
 
     override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return fakeData[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return position.toLong()
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 15
     }
 
 }
