6f3b62317f14d77ae43704b64f01ec1e4fdb28bb
liupeiqi <peiqial@outlook.com>
2017-04-24 09:05:59 -0400

    EventItem persistence object

--- app/src/main/java/guardiantech/com/cn/swedit/persistence/EventItem.kt
+++ app/src/main/java/guardiantech/com/cn/swedit/persistence/EventItem.kt
@@ -1,8 +1,31 @@
 package guardiantech.com.cn.swedit.persistence
 
+import com.j256.ormlite.field.DataType
+import com.j256.ormlite.field.DatabaseField
+import com.j256.ormlite.table.DatabaseTable
+import java.util.*
+
 /**
  * Created by liupeiqi on 2017/4/20.
  */
-class EventItem {
-//    private var id: Long
+@DatabaseTable(tableName = "events")
+class EventItem() {
+    @DatabaseField(id = true)
+    private lateinit var eventId: String
+    @DatabaseField
+    private lateinit var eventName: String
+    @DatabaseField
+    private lateinit var eventDescription: String
+    @DatabaseField(dataType = DataType.DATE_TIME)
+    private lateinit var eventTime: Date
+    @DatabaseField
+    private var eventStatus: Long = 0
+
+    constructor(eventId: String, eventName: String, eventDescription: String, eventTime: Date, eventStatus: Long) : this() {
+        this.eventId = eventId
+        this.eventName = eventName
+        this.eventDescription = eventDescription
+        this.eventTime = eventTime
+        this.eventStatus = eventStatus
+    }
 }
